47988dc76a8e5707304b9e19c445f5bf530fc96b
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-09-15 12:08:57 -0400

    updated momentum

--- src/defaultpackage/PizzaCost.java
+++ src/defaultpackage/PizzaCost.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package defaultpackage;
 
-/**
- *
- * @author cars0520
- */
+import java.util.Scanner;
+
+
 public class PizzaCost {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        final double labour = 1.5;
+        final double heat = 0.75;
     }
     
 }
