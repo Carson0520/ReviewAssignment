3e50980dd029cdec9568b672f486b24f2a0708eb
cars0520 <cars0520@ISS2TTPKH2.scdsb.pri>
2020-09-18 09:38:46 -0400

    in progress

--- src/part2/EmployeePay.java
+++ src/part2/EmployeePay.java
@@ -1,19 +1,32 @@
 
 package part2;
 
+import java.util.Scanner;
+
 
 public class EmployeePay {
 
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         int hours;
-        double wage;
+        double wage, pay, ot;
         System.out.println("Enter hours worked for Employee 1: ");
-        for (int wage = 0; hours < 40; hour++) {
-            
-            System.out.println("" + regpay);
+        hours = s.nextInt();
+        if (hours > 40 && hours < 70){
+            pay = 40 * 15.00;
+            ot = (hours - 40) * 22.50;
+            wage = pay + ot;
+            System.out.println("Regular pay $" + pay);
+            System.out.println("Overtime pay $" + ot);
+            System.out.println("Total pay $" + wage);
         }
-
+        else if(hours > 0 && hours < 70){
+            pay = hours * 15.00;
+            System.out.println("Total pay $" + pay);
+    }
+        else
+            System.out.println("Invalid, try again");
     }
     
 }
